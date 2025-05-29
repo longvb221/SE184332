6e635a1a16b659f755ed589a301ddf6a71cabb2f
Vu Bao Long <longvbse184332@fpt.edu.vn>
2025-05-29 22:42:29 +0700

    Add files via upload

---
+++ OOP4_Reuse_OOP2/MyUtils.cs
@@ -0,0 +1,42 @@
+﻿using OOP2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP4_Reuse_OOP2
+{
+    public static class MyUtils
+    {
+        public static int Tuoi(this Employee emp)
+        {
+            return DateTime.Now.Year - emp.Birthday.Year;
+        }
+
+  //viet mot method tra ve thang nay co phai thang sinh nhat ho hay khong
+        public static Boolean KiemTraSinhNhat(this Employee emp)
+        {
+            if (emp.Birthday.Year == DateTime.Now.Year)
+            {
+                return true;
+            }
+            else
+            {
+
+                return false;
+            }
+
+
+            }
+
+
+
+        }
+    } 
+
+
+
+
+
---
+++ OOP4_Reuse_OOP2/Program.cs
@@ -0,0 +1,21 @@
+﻿using OOP2;
+using OOP4_Reuse_OOP2;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+FulltimeEmployee fe = new FulltimeEmployee();
+fe.Id = 1;
+fe.Name = "hehehe";
+fe.IdCard = "3636";
+fe.Birthday = new DateTime(2004, 12, 12);
+Console.WriteLine("==>AGE = "+ fe.Tuoi());
+if (fe.KiemTraSinhNhat() == true)
+{
+    Console.WriteLine("la sinh nhat");
+}
+else(fe.KiemTraSinhNhat() == false)
+{
+    Console.WriteLine("no");
+}
+
